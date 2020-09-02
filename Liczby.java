f0bf26df474f26083fed3646eb71c599db650ff0
Krystian Wolski <46653110+Krystianxdj@users.noreply.github.com>
2020-09-02 15:01:48 +0100

    Create Liczby.java

---
+++ Liczby.java
@@ -0,0 +1,18 @@
+class Liczby {
+
+    int x;
+    int y;
+
+    Liczby(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    int dodaj() {
+        return x + y;
+    }
+    int odejmij(){
+        return  x-y;
+    }
+
+}
