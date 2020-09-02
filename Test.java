2b89aedda1f9550fcedc8eb74629170e5bed53ab
Krystian Wolski <46653110+Krystianxdj@users.noreply.github.com>
2020-09-02 15:06:16 +0100

    Delete Test.java

--- Test.java
+++
@@ -1,9 +0,0 @@
-public class Test {
-
-    public static void main(String[] args) {
-
-        Liczby liczby1 = new Liczby(10, 6);
-
-        System.out.println(liczby1.odejmij());  // lub dodaj()
-    }
-}
