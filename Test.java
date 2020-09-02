47504f172a8573c728a53ba1a0d361936b3a91d7
Krystian Wolski <46653110+Krystianxdj@users.noreply.github.com>
2020-09-02 15:03:23 +0100

    Create Test.java

---
+++ Test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by R3id4k on 30.10.2017.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Liczby liczby1 = new Liczby(10, 6);
+
+        System.out.println(liczby1.odejmij());  // lub dodaj()
+    }
+}
